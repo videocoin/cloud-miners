8aa6c23822501cb12c5583aa9e1edc9be5c2e703
Aleksandr Didenko <aa.didenko@yandex.com>
2019-11-18 18:49:16 +0300

    Add system info to miner response

--- service/rpc_util.go
+++ service/rpc_util.go
@@ -42,9 +42,22 @@ func (s *RPCServer) getTokenType(ctx context.Context) auth.TokenType {
 }
 
 func toMinerResponse(miner *Miner) *v1.MinerResponse {
+	info := &v1.SystemInfo{}
+
+	if cpuInfo, ok := miner.SystemInfo["cpu"]; ok {
+		info.CpuCores = int32(cpuInfo.(map[string]interface{})["cores"].(float64))
+		info.CpuFreq = int32(cpuInfo.(map[string]interface{})["freq"].(float64))
+	}
+
+	if memInfo, ok := miner.SystemInfo["memory"]; ok {
+		info.MemUsage = float32(memInfo.(map[string]interface{})["used"].(float64))
+		info.MemTotal = float32(memInfo.(map[string]interface{})["total"].(float64))
+	}
+
 	return &v1.MinerResponse{
-		Id:     miner.ID,
-		Name:   miner.Name,
-		Status: miner.Status,
+		Id:         miner.ID,
+		Name:       miner.Name,
+		Status:     miner.Status,
+		SystemInfo: info,
 	}
 }
