190611d516fcd7023f2b8b9c7b35320c0097e45e
Aleksandr Didenko <aa.didenko@yandex.com>
2020-05-01 01:09:34 +0300

    Fix access key field type

--- migrations/00004_add_access_key_field.sql
+++ migrations/00004_add_access_key_field.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE miners ADD `access_key` varchar(255) DEFAULT NULL;
+ALTER TABLE miners ADD `access_key` TEXT DEFAULT NULL;
 
 
 -- +goose Down
